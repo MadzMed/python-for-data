49e470ad39ac79de01f1390677d5e57355a9110a
mahdi Lamriben <mehdilmbn@gmail.com>
2020-08-16 19:11:41 +0200

    Monty hall via numpy

---
+++ montyhalltool.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+def game(strategy):
+    
+    return None
+    
+    
+def check_victory(decision, winning_door):
+    return decision == winning_door
+
+class Strategy(Enum):
+    CHANGE= 1
+    KEEP= 2
